7ab3b02e734a64704fdbba50176468219ac3d39e
Michał Skrzypek <skrzypekmichal@yahoo.com>
2018-08-24 20:40:52 +0200

    Changed sessionStorage to localStorage to remain valid token throughout different browser sessions

--- ui/src/shared/authentication/TokenStorage.ts
+++ ui/src/shared/authentication/TokenStorage.ts
@@ -9,16 +9,16 @@ export class TokenStorage {
   constructor() { }
 
   removeToken() {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.clear();
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.clear();
   }
 
   saveToken(token: string) {
-    window.sessionStorage.removeItem(TOKEN_KEY);
-    window.sessionStorage.setItem(TOKEN_KEY,  token);
+    window.localStorage.removeItem(TOKEN_KEY);
+    window.localStorage.setItem(TOKEN_KEY,  token);
   }
 
   getToken(): string {
-    return sessionStorage.getItem(TOKEN_KEY);
+    return window.localStorage.getItem(TOKEN_KEY);
   }
 }
